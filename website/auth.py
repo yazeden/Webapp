61761886faf610d7e90c3061731cc853f4379223
yazeden <eden.knapp@outlook.com>
2025-05-14 19:21:27 +0200

    py paginas

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -12,6 +12,6 @@ def logout():
 
 @auth.route('/sign-up')
 def signup():
-    return "<p>Sign Up</p>"
+    return render_template("signup.html")
 
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        pass
+    return render_template("signup.html")
+
+@views.route('/login')
+def login():
+    return render_template("login.html")
