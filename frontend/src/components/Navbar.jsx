d14f8126720d69e69cfd7d2f6847792feafad878
yazeden <eden.knapp@outlook.com>
2025-05-28 11:40:21 +0200

    components added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar() {
+    return (
+        <div className="site-header">
+            <div className="container">
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
