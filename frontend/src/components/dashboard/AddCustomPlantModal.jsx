cbd1c02e17bea39b09d6df0365796b031cbd0ffb
yazeden <eden.knapp@outlook.com>
2025-06-11 15:27:07 +0200

    import useeffect

--- frontend/src/components/dashboard/AddCustomPlantModal.jsx
+++ frontend/src/components/dashboard/AddCustomPlantModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 const AddCustomPlantModal = ({ isOpen, onClose, onPlantAdded }) => {
