0ed5e68acba5af52198234457c696dbc2e728933
Jouw Naam <yazeden>
2026-05-28 18:33:42 +0200

    signuppage same py as loginpage

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -118,7 +118,7 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-[#F5EEDE] flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-[#F5EEDE] flex items-center justify-center pt-24 md:pt-28 pb-12 px-4 sm:px-6 lg:px-8">
       <motion.div
         variants={formVariant}
         initial="hidden"
